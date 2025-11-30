f631e697b58b4dba4f51bef9528ee689c3da5b7e
Sh1ney <vilensky.net@mail.com>
2025-11-30 22:25:16 +0500

    first attempt

--- stack.cpp
+++ stack.cpp
@@ -1,48 +1,94 @@
 #include "stack.hpp"
-
+#include <map>
 #include <cstddef>
-
-// TODO: remove me
-#define UNUSED(VAR) (void)(VAR)
+#include <cstdlib>
+#include <cstring>
 
 namespace stack
 {
+    struct Element
+    {
+        void* data;
+        Element* next;
 
-Handle create()
-{
-    return -1;
+        Element(const void* const data, const std::size_t size, Element* next)
+        {
+            this->data = malloc(size);
+            memcpy(this->data, data, size);
+            this->next = next;
+        }
+
+        ~Element()
+        {
+            free(data);
+        }
+    };
+
+    struct Stack
+    {
+        Element* top;
+        std::size_t size;
+
+        Stack()
+        {
+            this->top = nullptr;
+            this->size = 0;
+        }
+
+        ~Stack()
+        {
+            while (this->size > 0)
+            {
+                Element* toDelete = this->top;
+                this->top = toDelete->next;
+                delete toDelete;
+                this->size--;
+            }
+        }
+    };
+
+    int stackCount = 0;
+    std::map<Handle, Stack*> handleMap;
+
+    Handle create()
+    {
+        Stack* stack = new Stack();
+        handleMap[stackCount] = stack;
+        return stackCount++;
+    }
+
+    void destroy(const Handle handle)
+    {
+        if (valid(handle))
+        {
+            delete handleMap[handle];
+        }
+    }
+
+    bool valid(const Handle handle)
+    {
+        return handleMap.find(handle) != handleMap.end();
+    }
+
+    std::size_t count(const Handle handle)
+    {
+        return handleMap[handle]->size;
+    }
+
+    void push(const Handle handle, const void* const data, const std::size_t size)
+    {
+        Stack* stack = handleMap[handle];
+        Element* element = new Element(data, size, stack->top);
+        stack->top = element;
+        stack->size++;
+    }
+
+    std::size_t pop(const Handle handle, void* const data, const std::size_t size)
+    {
+        Stack* stack = handleMap[handle];
+        memcpy(data, stack->top->data, size);
+        stack->top = stack->top->next;
+        stack->size--;
+        return size;
+    }
 }
-
-void destroy(const Handle handle)
-{
-    UNUSED(handle);
-}
-
-bool valid(const Handle handle)
-{
-    UNUSED(handle);
-    return false;
-}
-
-std::size_t count(const Handle handle)
-{
-    UNUSED(handle);
-    return 0u;
-}
-
-void push(const Handle handle, const void* const data, const std::size_t size)
-{
-    UNUSED(handle);
-    UNUSED(data);
-    UNUSED(size);
-}
-
-std::size_t pop(const Handle handle, void* const data, const std::size_t size)
-{
-    UNUSED(handle);
-    UNUSED(data);
-    UNUSED(size);
-    return 0u;
-}
-
-} // namespace stack
